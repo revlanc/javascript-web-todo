a60ab3af67c5c4f9ec9c11552c7c5465f1f593d8
AllenK <revenoir7@gmail.com>
2019-09-28 16:52:20 +0900

    style: TodoCounter 스타일 수정
    - 카운트 값 변경 시 폰트크기 대신 백그라운드 컬러를 변경하도록 수정

--- src/components/TodoCounter.jsx
+++ src/components/TodoCounter.jsx
@@ -6,38 +6,6 @@ import { makeDelay } from '../utils/myUtils';
 
 const { ANIMATE_DURATION, MAIN_COLOR } = CONFIGS;
 
-const Wrapper = styled.div`
-  display: flex;
-  justify-content: space-between;
-  width: 100%;
-  max-width: inherit;
-  min-width: inherit;
-  padding: 2rem;
-`;
-
-const Box = styled.div`
-  display: flex;
-  align-items: center;
-`;
-
-const Typography = styled.h3`
-  font-size: 2rem;
-  font-weight: bold;
-  margin: 1rem;
-`;
-
-const Circle = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border-radius: 50%;
-  border: 2px solid ${MAIN_COLOR};
-  width: 5rem;
-  height: 5rem;
-  font-size: ${props => (props.emphasize === true ? '4rem' : '2rem')};
-  transition: all ${ANIMATE_DURATION}ms;
-`;
-
 const TodoCounter = () => {
   const { todoCount, doneCount } = useTodoContext();
   const [emphasize, setEmphasize] = useState(false);
@@ -52,16 +20,33 @@ const TodoCounter = () => {
 
   return (
     <Wrapper>
-      <Box>
-        <Typography>Todo: </Typography>
-        <Circle emphasize={emphasize}>{todoCount}</Circle>
-      </Box>
-      <Box>
-        <Typography>Done: </Typography>
-        <Circle emphasize={emphasize}>{doneCount}</Circle>
-      </Box>
+      <Box emphasize={emphasize}>Todo: {todoCount}</Box>
+      <Box emphasize={emphasize}>Done: {doneCount}</Box>
     </Wrapper>
   );
 };
 
 export default TodoCounter;
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  max-width: inherit;
+  min-width: inherit;
+  padding: 1rem;
+  margin-bottom: 2rem;
+`;
+
+const Box = styled.div`
+  padding: 0.75rem 2rem;
+  width: 11.5rem;
+  border: 2px solid ${MAIN_COLOR};
+  border-radius: 5px;
+
+  font-size: 2rem;
+  font-weight: 400;
+  background: ${props => (props.emphasize === true ? MAIN_COLOR : '#fff')};
+  color: ${props => (props.emphasize === true ? '#fff' : '#000')};
+  transition: all ${ANIMATE_DURATION}ms;
+`;
